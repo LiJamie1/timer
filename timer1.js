a274d4c197a732460ec273860df6b5a26a4865ef
Jamie Li <lijamie19828@gmail.com>
2021-11-03 00:17:26 +0000

    added filtering function to skip negative ints and non number inputs

--- timer1.js
+++ timer1.js
@@ -3,9 +3,12 @@ const args = process.argv.slice(2);
 const alarm = function(args) {
   let arrOfTimes = args.map(Number);
   for (let i = 0; i < arrOfTimes.length; i++) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, arrOfTimes[i] * 1000);
+    if (arrOfTimes[i] > 0 &&  Number.isInteger(arrOfTimes[i])) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+        console.log(`alarm rang at ${arrOfTimes[i]} seconds`)
+      }, arrOfTimes[i] * 1000);
+    }
   }
 };
 
